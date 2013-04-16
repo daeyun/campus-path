79e8b3e892d437276172db3d46a614c8ac1e7b5f
Daeyun Shin <daeyunshin@gmail.com>
2013-04-16 13:17:55 -0500

    RouteUnit ndb model

--- models.py
+++ models.py
@@ -3,4 +3,10 @@ from google.appengine.api import users
 from google.appengine.ext.webapp import template
 from google.appengine.ext import ndb
 
-# NDB Models here
+
+class RouteUnit(ndb.Model):
+    transportation = ndb.StringProperty()
+    start = ndb.GeoPtProperty()
+    end = ndb.GeoPtProperty()
+    duration = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty()
