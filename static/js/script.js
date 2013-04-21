132c320579a9fd271b3a173e3a1b09e3fbe9f84e
Daeyun Shin <daeyunshin@gmail.com>
2013-04-21 11:54:33 -0500

    a map window pops up when you click "locate on map"

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,57 @@
 $(function() {
+
+
+    var map;
+    function initialize() {
+        var mapOptions = {
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+        // Try HTML5 geolocation
+        if(navigator.geolocation) {
+            console.log("geolocation works");
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = new google.maps.LatLng(position.coords.latitude,
+                    position.coords.longitude);
+
+                var infowindow = new google.maps.InfoWindow({
+                    map: map,
+                    position: pos,
+                    content: 'Location found using HTML5.'
+                });
+
+                map.setCenter(pos);
+            }, function() {
+                handleNoGeolocation(true);
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleNoGeolocation(false);
+        }
+    }
+    function handleNoGeolocation(errorFlag) {
+        if (errorFlag) {
+            var content = 'Error: The Geolocation service failed.';
+        } else {
+            var content = 'Error: Your browser doesn\'t support geolocation.';
+        }
+
+        var options = {
+            map: map,
+            position: new google.maps.LatLng(60, 105),
+            content: content
+        };
+
+        var infowindow = new google.maps.InfoWindow(options);
+        map.setCenter(options.position);
+    }
+
+    $("a.locateFrom").click(function(e){
+        e.preventDefault();
+        $("#map-window").show();
+        initialize();
+    });
+
 });
