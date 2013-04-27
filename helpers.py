4ad4d9ee18a1d729ea59f9ddf41720daa3d9672a
Kevin Larson <kevinlarson1@gmail.com>
2013-04-27 13:32:52 -0500

    Added geomodel and backbone support the populate function isn't currently working

--- helpers.py
+++ helpers.py
@@ -6,6 +6,8 @@ from models import *
 import sys
 import csv
 
+from geo.geomodel import GeoModel
+
 
 def populate():
     try:
@@ -18,12 +20,14 @@ def populate():
             row = row[0].split(",")
             lat = row[0][1:]
             lon = row[1][:-1]
-            locations.append(lat+','+lon)
+            parking_meter = ParkingMeter(location=ndb.GeoPt(lat, lon), 
+                                         time_limit=60,
+                                         time_per_quarter=15,
+                                         enforcement_start=8,
+                                         enforcement_end=6,
+                                         congestion=1)
+            parking_meter.put()
 
-        print locations
-        p = ParkingMeters(locations=locations)
-        k = p.put()
-        print k
 
     except IOError as (errno, strerror):
             print "I/O error({0}): {1}".format(errno, strerror)
--- models.py
+++ models.py
@@ -3,6 +3,49 @@ from google.appengine.api import users
 from google.appengine.ext.webapp import template
 from google.appengine.ext import ndb
 
+from geo.geomodel import GeoModel
 
-class ParkingMeters(ndb.Model):
-    locations = ndb.JsonProperty()
+class ParkingMeter(GeoModel):
+    #in minutes
+    time_limit = ndb.IntegerProperty()
+
+    #in minutes per quarter
+    time_per_quarter = ndb.IntegerProperty()
+
+    #in 24h times
+    enforcement_start = ndb.IntegerProperty()
+
+    #in 24h times
+    enforcement_end = ndb.IntegerProperty()
+
+    #on a scale of 1-10
+    congestion = ndb.IntegerProperty()
+
+    @staticmethod
+    def public_attributes():
+        """Returns a set of simple attributes on public school entities."""
+        return [
+            'time_limit', 'time_per_quarter', 'enforcement_start', 'enforcement_end', 'congestion'
+            ]
+
+    def _get_latitude(self):
+        return self.location.lat if self.location else None
+
+    def _set_latitude(self, lat):
+        if not self.location:
+            self.location = db.GeoPt()
+
+        self.location.lat = lat
+
+    latitude = property(_get_latitude, _set_latitude)
+
+    def _get_longitude(self):
+        return self.location.lon if self.location else None
+
+    def _set_longitude(self, lon):
+        if not self.location:
+            self.location = db.GeoPt()
+
+        self.location.lon = lon
+
+    longitude = property(_get_longitude, _set_longitude)
