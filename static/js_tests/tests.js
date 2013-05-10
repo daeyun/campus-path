f48c2e99ce016013d9dffe6c134738e3c7c94233
Daeyun Shin <daeyunshin@gmail.com>
2013-05-10 00:30:37 -0500

    made a test case slightly more thorough

--- static/js_tests/tests.js
+++ static/js_tests/tests.js
@@ -16,6 +16,22 @@ test( "Meter Display Test", function() {
     meter_text = meterDisplay(meter_info);
 
     ok( meter_text == "60 minute parking<br>Meters enforced from 8AM to 6AM<br>$1.00 per hour<br>This meter is likely unoccupied<br>", "Passed!" );
+
+
+    var meter_info = {
+        'congestion': 10,
+        'enforcement_end': 7,
+        'enforcement_start': 9,
+        'key': 6715817022455808,
+        'lat': 40.1079383377,
+        'lon': -88.2291651578,
+        'time_limit': 45,
+        'time_per_quarter': 7.5,
+    }
+    meter_text = meterDisplay(meter_info);
+    console.log(meter_text);
+
+    ok( meter_text == "45 minute parking<br>Meters enforced from 9AM to 7AM<br>$2.14.285714285714278 per hour<br>This meter is frequently occupied<br>", "Passed!" );
 });
 
 test( "URL Encode Test", function() {
