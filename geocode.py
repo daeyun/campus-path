93dff25bf1448584ebf03c55e99bf9e5668235fa
Kevin Larson <kevinlarson1@gmail.com>
2013-05-09 22:51:44 -0500

    Following MIT liscence requirement of geopy code in geocode.py

--- geocode.py
+++ geocode.py
@@ -5,6 +5,29 @@ from urllib2 import urlopen
 
 #get_encoding, decode_page, check_status, and geocode are largely 
 #from the GoogleV3 module in the geopy library
+#Per MIT liscence, we have included the following from geopy
+
+#Copyright (c) 2006-2010 Brian Beck
+#Copyright (c) 2010-2012 GeoPy Project and individual contributors.
+#All rights reserved.
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy of 
+#this software and associated documentation files (the "Software"), to deal in 
+#the Software without restriction, including without limitation the rights to 
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+#of the Software, and to permit persons to whom the Software is furnished to do 
+#so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all 
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+#SOFTWARE.
 
 def get_encoding(page, contents=None):
     charset = page.headers.getparam("charset") or None
